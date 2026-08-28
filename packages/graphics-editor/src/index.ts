a927f71e95b780d5eacff119e12c005f93125306
Juha Itäleino <jsilvanus@gmail.com>
2026-08-28 15:49:26 +0300

    feat: export wegra serialization API

--- packages/graphics-editor/src/index.ts
+++ packages/graphics-editor/src/index.ts
@@ -1,6 +1,8 @@
 export type { GraphicsAsset, GraphicsDocument, GraphicsEditorProps, Layer, LayerType, PathCommand, PathNode, Point } from "./types";
 export { GraphicsEditor, defaultGraphicsDocument } from "./GraphicsEditor";
 export { GRAPHICS_DOCUMENT_VERSION, serializeGraphicsDocument, deserializeGraphicsDocument } from "./serialization";
+export { serializeWegra, deserializeWegra } from "./wegra";
+export type { WegraManifest, WegraProject } from "./wegra";
 export { linePath, orthogonalPoint, pathCommandsToD, roundedRectPath, nodesToD, mirrorHandle } from "./geometry";
 export { roundedPolygonNodes } from "./geometry/rounded";
 export { exportSvg, importSvg } from "./svg";
