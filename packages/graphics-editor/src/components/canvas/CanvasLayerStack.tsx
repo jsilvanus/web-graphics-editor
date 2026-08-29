a0974e4b9b0ac637b222634c85ad664e6ee3f750
Juha Itäleino <jsilvanus@gmail.com>
2026-08-29 12:09:28 +0300

    Pass current timeline time to 3D views

--- packages/graphics-editor/src/components/canvas/CanvasLayerStack.tsx
+++ packages/graphics-editor/src/components/canvas/CanvasLayerStack.tsx
@@ -3,31 +3,8 @@ import type { Graphics3DView, Graphics3DWorld, Layer, PathNode } from "../../typ
 import { getRootLayers } from "../../layer-tree";
 import { CanvasLayer } from "./CanvasLayer";
 
-export interface CanvasLayerStackProps {
-  layers: Layer[];
-  selectedIds: Set<string>;
-  worlds3d: Graphics3DWorld[];
-  views3d: Graphics3DView[];
-  onLayerPointerDown: (event: ReactPointerEvent, id: string, kind: "move" | "resize" | "rotate", handle?: string) => void;
-  onSelectLayer?: (id: string, additive?: boolean) => void;
-  onPathNodes?: (id: string, nodes: PathNode[]) => void;
-}
+export interface CanvasLayerStackProps { layers: Layer[]; selectedIds: Set<string>; worlds3d: Graphics3DWorld[]; views3d: Graphics3DView[]; currentTime?: number; onLayerPointerDown: (event: ReactPointerEvent, id: string, kind: "move" | "resize" | "rotate", handle?: string) => void; onSelectLayer?: (id: string, additive?: boolean) => void; onPathNodes?: (id: string, nodes: PathNode[]) => void }
 
-export const CanvasLayerStack: FC<CanvasLayerStackProps> = ({ layers, selectedIds, worlds3d, views3d, onLayerPointerDown, onSelectLayer, onPathNodes }) => (
-  <>{getRootLayers(layers).map(layer => (
-    <CanvasLayer
-      key={layer.id}
-      layer={layer}
-      layers={layers}
-      selected={selectedIds.has(layer.id)}
-      selectedIds={selectedIds}
-      multiSelected={selectedIds.size > 1}
-      worlds3d={worlds3d}
-      views3d={views3d}
-      onSelectLayer={onSelectLayer}
-      onPointerDown={(event, kind, handle) => onLayerPointerDown(event, layer.id, kind, handle)}
-      onLayerPointerDown={onLayerPointerDown}
-      onNodes={nodes => onPathNodes?.(layer.id, nodes)}
-    />
-  ))}</>
+export const CanvasLayerStack: FC<CanvasLayerStackProps> = ({ layers, selectedIds, worlds3d, views3d, currentTime = 0, onLayerPointerDown, onSelectLayer, onPathNodes }) => (
+  <>{getRootLayers(layers).map(layer => <CanvasLayer key={layer.id} layer={layer} layers={layers} selected={selectedIds.has(layer.id)} selectedIds={selectedIds} multiSelected={selectedIds.size > 1} worlds3d={worlds3d} views3d={views3d} currentTime={currentTime} onSelectLayer={onSelectLayer} onPointerDown={(event, kind, handle) => onLayerPointerDown(event, layer.id, kind, handle)} onLayerPointerDown={onLayerPointerDown} onNodes={nodes => onPathNodes?.(layer.id, nodes)} />)}</>
 );
